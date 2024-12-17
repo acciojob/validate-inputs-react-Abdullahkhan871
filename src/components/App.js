e4939c633922720c5f79334dbf87ef30f6cca05b
Abdullah khan <akhan283785@gmail.com>
2024-12-17 13:17:13 +0530

    took me 02:55:50 hours

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import FormValidation from "./FormValidation";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <FormValidation />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/FormValidation.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+
+const FormValidation = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    address: "",
+    email: "",
+    mobile: "",
+  });
+  const { name, address, email, mobile } = userData;
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorAddress, setErrorAddress] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorMobile, setErrorMobile] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let regName = /[^a-z]/gi;
+    let regAdd = /[^a-z0-9\s]/gi;
+
+    setErrorMessage("");
+    setErrorAddress("");
+    setErrorEmail("");
+    setErrorMobile("");
+
+    if (
+      !name.length > 0 &&
+      !address.length > 0 &&
+      !email.length > 0 &&
+      !mobile.length > 0
+    ) {
+      setErrorMessage("Please Fill input");
+      setErrorAddress("Please Fill input");
+      setErrorEmail("Please Fill input");
+      setErrorMobile("Please Fill input");
+      return;
+    }
+
+    if (regName.test(name)) {
+      setErrorMessage("Name should contain only letters");
+    }
+    if (regAdd.test(address)) {
+      setErrorAddress("Address should not contain special characters");
+    }
+    if (!/(.com)/gi.test(email) && !/[@]/gi.test(email)) {
+      setErrorEmail("Email should contain @ and .com");
+    }
+
+    if (/[a-z]/gi.test(mobile) || mobile.length < 10) {
+      setErrorMobile("Mobile number should not be more than 10 characters");
+    }
+
+    // setUserData({
+    //   name: "",
+    //   address: "",
+    //   email: "",
+    //   mobile: "",
+    // });
+  }
+
+  function submitValue(e) {
+    setErrorMessage("");
+    setErrorAddress("");
+    setErrorEmail("");
+    setErrorMobile("");
+    let key = e.target.name;
+    let value = e.target.value;
+    setUserData((prev) => ({ ...prev, [key]: value }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Name</label>
+      <input type="text" name="name" value={name} onChange={submitValue} />
+      {errorMessage} <br /> <br />
+      <label>Address</label>
+      <input
+        type="text"
+        name="address"
+        value={address}
+        onChange={submitValue}
+      />
+      {errorAddress} <br /> <br />
+      <label>Email</label>
+      <input type="text" name="email" value={email} onChange={submitValue} />
+      {errorEmail} <br /> <br />
+      <label>Mobile</label>
+      <input type="text" name="mobile" value={mobile} onChange={submitValue} />
+      {errorMobile} <br /> <br />
+      <input type="submit" value={"Submit"} />
+    </form>
+  );
+};
+
+export default FormValidation;
